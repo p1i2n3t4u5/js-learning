c1cf5705700f64279e7e90b196da700b76ff69dc
AKSAHA <sheol.aks@gmail.com>
2019-02-06 15:51:17 +0530

    Updated composition and unit test

--- src/Currying/currying.js
+++ src/Currying/currying.js
@@ -17,50 +17,8 @@ function curry() {
 
 
 
-function negetive2(x){
-    return x - 2;
-}
-
-function getMajicNumber(x) {
-    return function () {
-        x = x + 2;
-        return function(){
-           return x = x * 2;
-        }
-    }
-}
-
-// Point Free style and function composition
-const addVal = x => x + 2;
-const multiplyVal = y => y * 2;
-const getMajicNumberV2 = n => multiplyVal(addVal(2));
-
-// Creating compose function
-const compose = (...fns) => x => fns.reduceRight((y, f) => f(y), x);
-const getMajicNumberV3 = compose(multiplyVal,addVal);
-
-
-// COMPOSITION
-const barker = (state) => ({  bark: () => 'Woof, I am ' +  state.name });
-const murderDog = (name) => {
-    let state = {
-        name,
-        speed: 40,
-        postion: 0
-    };
-
-    return Object.assign({}, barker(state));
-
-}
-
 module.exports = {
     addition: addition,
     curry: curry,
-
-    getMajicNumber: getMajicNumber,
-    getMajicNumberV2: getMajicNumberV2,
-    getMajicNumberV3: getMajicNumberV3,
-
-    murderDog: murderDog,
     // add2Multiply3: add2Multiply3
 }
